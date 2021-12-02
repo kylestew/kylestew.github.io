8aa883ea4a4508d6c51a222114f81cbf2aa105cf
Kyle Stewart <kylestew@gmail.com>
2021-12-01 20:46:20 -0700

    ...

--- scripts/webgl-demo/hello.js
+++ scripts/webgl-demo/hello.js
@@ -1,8 +1,27 @@
-import * as THREE from "../../weg_modules/three";
-
-// const canvas = document.querySelector("#gl-canvas");
-// const renderer = new THREE.WebGLRenderer({ canvas });
-// console.log(canvas, renderer);
+import { vec3 } from "/web_modules/gl-matrix.js";
+import { random } from "/web_modules/canvas-sketch-util.js";
 
 // import createCamera from "../../web_modules/perspective-camera.js";
 // console.log(createCamera);
+
+const createMesh = () => {
+  // crystal mesh is a convex hull of N random points on a sphere
+  const pointCount = 10;
+  const radius = 1;
+  const positions = Array.from(new Array(pointCount)).map(() => {
+    return random.onSphere(radius);
+  });
+
+  // center the mesh by finding its "centroid"
+  const centroid = positions.reduce(
+    (sum, pos) => {
+      return vec3.add(sum, sum, pos);
+    },
+    [0, 0, 0]
+  );
+
+  console.log(centroid);
+};
+
+let mesh = createMesh();
+console.log(mesh);
--- web_modules/perspective-camera.js
+++ web_modules/perspective-camera.js
@@ -1,3 +1,6 @@
+import cameraProject_1 from './camera-project.js';
+import { d as defined } from './common/index-7ecbc9af.js';
+
 function ToObject(val) {
 	if (val == null) {
 		throw new TypeError('Object.assign cannot be called with null or undefined');
@@ -327,83 +330,6 @@ Ray.prototype.intersectsTriangleCell = function (cell, positions) {
   return this.intersectsTriangle(tmpTriangle)
 };
 
-var transformMat4_1 = transformMat4;
-
-/**
- * Transforms the vec4 with a mat4.
- *
- * @param {vec4} out the receiving vector
- * @param {vec4} a the vector to transform
- * @param {mat4} m matrix to transform with
- * @returns {vec4} out
- */
-function transformMat4 (out, a, m) {
-  var x = a[0], y = a[1], z = a[2], w = a[3];
-  out[0] = m[0] * x + m[4] * y + m[8] * z + m[12] * w;
-  out[1] = m[1] * x + m[5] * y + m[9] * z + m[13] * w;
-  out[2] = m[2] * x + m[6] * y + m[10] * z + m[14] * w;
-  out[3] = m[3] * x + m[7] * y + m[11] * z + m[15] * w;
-  return out
-}
-
-var set_1 = set;
-
-/**
- * Set the components of a vec4 to the given values
- *
- * @param {vec4} out the receiving vector
- * @param {Number} x X component
- * @param {Number} y Y component
- * @param {Number} z Z component
- * @param {Number} w W component
- * @returns {vec4} out
- */
-function set (out, x, y, z, w) {
-  out[0] = x;
-  out[1] = y;
-  out[2] = z;
-  out[3] = w;
-  return out
-}
-
-var NEAR_RANGE = 0;
-var FAR_RANGE = 1;
-var tmp4 = [0, 0, 0, 0];
-
-var cameraProject_1 = cameraProject;
-function cameraProject (out, vec, viewport, combinedProjView) {
-  var vX = viewport[0],
-    vY = viewport[1],
-    vWidth = viewport[2],
-    vHeight = viewport[3],
-    n = NEAR_RANGE,
-    f = FAR_RANGE;
-
-  // convert: clip space -> NDC -> window coords
-  // implicit 1.0 for w component
-  set_1(tmp4, vec[0], vec[1], vec[2], 1.0);
-
-  // transform into clip space
-  transformMat4_1(tmp4, tmp4, combinedProjView);
-
-  // now transform into NDC
-  var w = tmp4[3];
-  if (w !== 0) { // how to handle infinity here?
-    tmp4[0] = tmp4[0] / w;
-    tmp4[1] = tmp4[1] / w;
-    tmp4[2] = tmp4[2] / w;
-  }
-
-  // and finally into window coordinates
-  // the foruth component is (1/clip.w)
-  // which is the same as gl_FragCoord.w
-  out[0] = vX + vWidth / 2 * tmp4[0] + (0 + vWidth / 2);
-  out[1] = vY + vHeight / 2 * tmp4[1] + (0 + vHeight / 2);
-  out[2] = (f - n) / 2 * tmp4[2] + (f + n) / 2;
-  out[3] = w === 0 ? 0 : 1 / w;
-  return out
-}
-
 var projectMat4 = project;
 
 /**
@@ -531,7 +457,7 @@ var cameraLookAt = function (direction, up, position, target) {
   }
 };
 
-var set_1$1 = set$1;
+var set_1 = set;
 
 /**
  * Set the components of a vec3 to the given values
@@ -542,7 +468,7 @@ var set_1$1 = set$1;
  * @param {Number} z Z component
  * @returns {vec3} out
  */
-function set$1(out, x, y, z) {
+function set(out, x, y, z) {
     out[0] = x;
     out[1] = y;
     out[2] = z;
@@ -551,8 +477,8 @@ function set$1(out, x, y, z) {
 
 var cameraPickingRay = createPickRay;
 function createPickRay(origin, direction, point, viewport, invProjView) {
-  set_1$1(origin, point[0], point[1], 0);
-  set_1$1(direction, point[0], point[1], 1);
+  set_1(origin, point[0], point[1], 0);
+  set_1(direction, point[0], point[1], 1);
   cameraUnproject(origin, origin, viewport, invProjView);
   cameraUnproject(direction, direction, viewport, invProjView);
   subtract_1(direction, direction, origin);
@@ -715,9 +641,9 @@ var cameraBase = function cameraBase (opt) {
   }
 
   function identity () {
-    set_1$1(camera.position, 0, 0, 0);
-    set_1$1(camera.direction, 0, 0, -1);
-    set_1$1(camera.up, 0, 1, 0);
+    set_1(camera.position, 0, 0, 0);
+    set_1(camera.direction, 0, 0, -1);
+    set_1(camera.up, 0, 1, 0);
     identity_1(camera.view);
     identity_1(camera.projection);
     identity_1(camera.projView);
@@ -753,12 +679,6 @@ var cameraBase = function cameraBase (opt) {
     project: project,
     unproject: unproject
   })
-};
-
-var defined = function () {
-    for (var i = 0; i < arguments.length; i++) {
-        if (arguments[i] !== undefined) return arguments[i];
-    }
 };
 
 var perspective_1 = perspective;
